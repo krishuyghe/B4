518ab4bdd84648a727c75747063996187ce01455
krishuyghe <36008639+krishuyghe@users.noreply.github.com>
2018-10-25 21:54:36 +0200

    laptimer

--- B4.PE2.HuygheK/B4.PE2.HuygheK/B4.PE2.HuygheK/LapTimer.xaml.cs
+++ B4.PE2.HuygheK/B4.PE2.HuygheK/B4.PE2.HuygheK/LapTimer.xaml.cs
@@ -12,20 +12,58 @@ namespace B4.PE2.HuygheK
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class LapTimer : ContentPage
 	{
-		public LapTimer ()
-		{
-			InitializeComponent ();
-		}
-	    private async void BtnStart_Clicked(object sender, EventArgs e)
+        DateTime t = DateTime.Now;
+        public string tijd = "";
+        public bool wissel = false;
+        public int lapTeller = 0;
+        public string lapText = "";
+        public LapTimer()
+        {
+            InitializeComponent();
+            
+        }
+        private async void BtnStart_Clicked(object sender, EventArgs e)
 	    {
-	        
+            Device.StartTimer(TimeSpan.FromMilliseconds(10), () =>
+            {
+                
+                TimeSpan span = (t - DateTime.Now);
+                if (wissel == true)
+                {
+                    tijd = span.ToString("c");
+                    Tijdmelding.Text = tijd;
+                }
+                return true;
+            });
+            if (wissel == true)
+            {
+                (sender as Button).Text = "Start";
+                t = DateTime.Now;
+                lapTeller++;
+                lapText = lapText + "lap " + lapTeller.ToString() + " Duur:" + tijd + "\n";
+                LapTekst.Text = lapText;
+                wissel = false;
+            }
+            else
+            {
+                lapText = "";
+                lapTeller = 0;
+                LapTekst.Text = lapText;
+                string a = tijd;
+                (sender as Button).Text = "Stop";
+                wissel = true;
+                Tijdmelding.Text = tijd;
+            }
 
-	    }
+        }
 	    private async void BtnLap_Clicked(object sender, EventArgs e)
 	    {
+            lapTeller++;
+            lapText = lapText + "lap "+ lapTeller.ToString() + " Duur:" + tijd + "\n";
+            LapTekst.Text = lapText;
+            t = DateTime.Now;
 
-
-	    }
+        }
         private async void BtnStop_Clicked(object sender, EventArgs e)
 	    {
 
---
+++ B4.PE2.HuygheK/B4.PE2.HuygheK/B4.PE2.HuygheK/classe/TijdMeter.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace B4.PE2.HuygheK.classe
+{
+
+    public class TijdMeter 
+    {
+           
+    }
+}
